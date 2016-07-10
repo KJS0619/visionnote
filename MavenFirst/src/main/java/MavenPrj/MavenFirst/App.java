32b0800b4614cae0954e752b87dea986c2bbf518
KJS0619 <user@user-PC>
2016-07-10 10:19:32 +0900

    커밋해 주세요

---
+++ MavenFirst/src/main/java/MavenPrj/MavenFirst/App.java
@@ -0,0 +1,15 @@
+package MavenPrj.MavenFirst;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello JSWorld!" );
+        System.out.println( "Hello JSWorld2!" );        
+    }
+}
